102550fbe1d0bfbd6d18facb0d38dc20e4f0ad04
Gustavo Siciliano <gussiciliano@gmail.com>
2023-06-11 22:18:11 -0300

    done transfer but with errors

--- agroecologia-iot-dispositivo/main/data_transmitter.c
+++ agroecologia-iot-dispositivo/main/data_transmitter.c
@@ -66,37 +66,11 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "/metrics/22", "temp: 18.5", 0, 1, 0);
+        msg_id = esp_mqtt_client_publish(client, "/metrics/hello", "hello from 123", 0, 1, 0);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
@@ -114,7 +88,48 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
-static void mqtt_app_start(void)
+static void mqtt_app_send_metric(char * metric)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = BROKER_URI,
+        //.username = MOSQUITO_USER_NAME,
+        //.password = MOSQUITO_USER_PASSWORD,
+    };
+#if CONFIG_BROKER_URL_FROM_STDIN
+    char line[128];
+
+    if (strcmp(mqtt_cfg.uri, "FROM_STDIN") == 0) {
+        int count = 0;
+        printf("Please enter url of mqtt broker\n");
+        while (count < 128) {
+            int c = fgetc(stdin);
+            if (c == '\n') {
+                line[count] = '\0';
+                break;
+            } else if (c > 0 && c < 127) {
+                line[count] = c;
+                ++count;
+            }
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+        mqtt_cfg.uri = line;
+        printf("Broker url: %s\n", line);
+    } else {
+        ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
+        abort();
+    }
+#endif /* CONFIG_BROKER_URL_FROM_STDIN */
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    int msg_id;
+    ESP_LOGI(TAG, "Publish");
+    msg_id = esp_mqtt_client_publish(client, "/metrics/123", metric, 0, 1, 0);
+    ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+    esp_mqtt_client_start(client);
+}
+
+static void mqtt_app_start()
 {
     esp_mqtt_client_config_t mqtt_cfg = {
         .uri = BROKER_URI,
@@ -152,8 +167,7 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
-static void transfer_by_mqtt(void)
-{
+static void checks(void){
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
@@ -175,8 +189,12 @@ static void transfer_by_mqtt(void)
      * examples/protocols/README.md for more information about this function.
      */
     //ESP_ERROR_CHECK(example_connect());
+}
 
-    mqtt_app_start();
+static void transfer_by_mqtt(char * metric)
+{
+    checks();
+    mqtt_app_send_metric(metric);
 }
 
 
@@ -220,7 +238,7 @@ void appendToTxBuffer(char * txBuffer, char *name, char *value)
 	sprintf(&txBuffer[len],"\"%s\":%s,",name,value);
 }
 
-void dataTransmitterTask(void * parameter)
+/*void dataTransmitterTask(void * parameter)
 {
 	char * txBuffer = malloc(TX_BUFF_SIZE);
 	while(1)
@@ -254,18 +272,18 @@ void dataTransmitterTask(void * parameter)
 void dataTransmitterStart(void)
 {
 	xTaskCreate(dataTransmitterTask,"dataTxTask", 10000, NULL, 1, NULL);
-}
+}*/
 
 void dataTransmitterPrintTask(void * parameter)
 {
 	
   printf("-------------------------TEST PRINT\n");
 	char * txBuffer = malloc(TX_BUFF_SIZE);
-	//while(1)
-	//{
-	//	vTaskDelay(1000 / portTICK_PERIOD_MS);
-	//	wifi_wait_connected();
-	  strcpy(txBuffer,"{");
+	while(1)
+	{
+		vTaskDelay(15000 / portTICK_PERIOD_MS);
+        wifi_wait_connected();
+        strcpy(txBuffer,"{");
 		for(int i=0;i<MAX_SENSORS;i++)
 		{
 			if(registeredSensors[i].sensorRead != NULL)
@@ -276,14 +294,20 @@ void dataTransmitterPrintTask(void * parameter)
 			}
 		}
 		txBuffer[strlen(txBuffer) -1] = '}'; //cambiamos la , por }
-		printf("Temperature is ");
+		printf("---Temperature is:");
 		printf(txBuffer);
-		printf("Temperature DONE");
-	//}
+        transfer_by_mqtt(txBuffer);
+	}
+}
+
+void connectMQTT(void)
+{
+	checks();
+    mqtt_app_start();
 }
 
 void dataTransmitterPrintStart(void)
 {
-	//xTaskCreate(dataTransmitterPrintTask,"dataTxTask", 10000, NULL, 1, NULL);
-	transfer_by_mqtt();
+    connectMQTT();
+	xTaskCreate(dataTransmitterPrintTask,"dataTxTask", 10000, NULL, 1, NULL);
 }
