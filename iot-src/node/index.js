9e164be4d49a66996b41c0c94ea99e5c8490fc59
Gustavo Siciliano <gussiciliano@gmail.com>
2023-06-16 17:33:17 -0300

    insert-metrics-into-db

--- iot-src/node/index.js
+++ iot-src/node/index.js
@@ -18,17 +18,39 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   console.log("Topic: " + topic.toString());
   const objResponse = JSON.parse(message.toString());
-  console.log("Temperatura ambiente: " + objResponse.ta);
-  console.log("Humedad relativa: " + objResponse.hr);
-
-  /*utils.query('INSERT INTO `metric_reading` (`reading_date`, `value`, `value_type`, `metric_type_id`, `sector_id`) VALUES (?,?,?,?,?)',
-      ["2020-10-10", 20, "porcentaje", "hum", 1],
+  if(objResponse.hasOwnProperty('ta')){
+    console.log("Temperatura ambiente: " + objResponse.ta);
+    utils.query('INSERT INTO `metric_reading` (`reading_date`, `value`, `value_type`, `metric_type_id`, `sector_id`) VALUES (?,?,?,?,?)',
+      ["2020-10-10", objResponse.ta, "porcentaje", "ta", 1],
       function(err, rta, field) {
-          if (err) {
-              return;
-          }
+        if (err) {
+          return;
+        }
       }
-  );*/
+    );
+  }
+  if(objResponse.hasOwnProperty('hr')){
+    console.log("Temperatura ambiente: " + objResponse.hr);
+    utils.query('INSERT INTO `metric_reading` (`reading_date`, `value`, `value_type`, `metric_type_id`, `sector_id`) VALUES (?,?,?,?,?)',
+      ["2020-10-10", objResponse.hr, "porcentaje", "hr", 1],
+      function(err, rta, field) {
+        if (err) {
+          return;
+        }
+      }
+    );
+  }
+  if(objResponse.hasOwnProperty('hs')){
+    console.log("Temperatura ambiente: " + objResponse.hs);
+    utils.query('INSERT INTO `metric_reading` (`reading_date`, `value`, `value_type`, `metric_type_id`, `sector_id`) VALUES (?,?,?,?,?)',
+      ["2020-10-10", objResponse.hs, "porcentaje", "hs", 1],
+      function(err, rta, field) {
+        if (err) {
+          return;
+        }
+      }
+    );
+  }
 })
 
 app.use(express.json());
